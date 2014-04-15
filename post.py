c326bb2889a3e73d67c305f08d1a0507be0106ad
Grant Regimbal <Regimbal.grant@gmail.com>
2014-04-14 21:01:44 -0700

    added a date module

--- post.py
+++ post.py
@@ -1,10 +1,11 @@
 import pickle
+import datetime
 
 
 posts = []
 #create your posts here
-hello = {"title" : "Hello, World","text" : "This is a test post don't be alarmed"}
-goodbye = {"title": "Goodbye, World","text":"This is a goodbye post, Goodbye"}
+hello = {"title" : "Hello, World","text" : "This is a test post don't be alarmed","date": datetime.date(2014,4,14)}
+goodbye = {"title": "Goodbye, World","text":"This is a goodbye post, Goodbye","date": datetime.date(2014,4,14)}
 
 #end posts append them here
 posts.append(hello)
